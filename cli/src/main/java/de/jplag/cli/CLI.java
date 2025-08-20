6ae19e405990391c042446a02aebeb4c7eeff647
b0ink <40929320+b0ink@users.noreply.github.com>
2025-08-20 18:45:30 +1000

    Run after parse

--- cli/src/main/java/de/jplag/cli/CLI.java
+++ cli/src/main/java/de/jplag/cli/CLI.java
@@ -54,11 +54,11 @@ public final class CLI {
     public void executeCli() throws ExitException, IOException {
         logger.debug("Your version of JPlag is {}", JPlag.JPLAG_VERSION);
         logger.info("skipVersionCheck value is {}", this.inputHandler.getCliOptions().advanced.skipVersionCheck);
-        if (!this.inputHandler.getCliOptions().advanced.skipVersionCheck) {
-            JPlagVersionChecker.printVersionNotification();
-        }
 
         if (!this.inputHandler.parse()) {
+            if (!this.inputHandler.getCliOptions().advanced.skipVersionCheck) {
+                JPlagVersionChecker.printVersionNotification();
+            }
             CollectedLogger.setLogLevel(this.inputHandler.getCliOptions().advanced.logLevel);
             ProgressBarLogger.setProgressBarProvider(new CliProgressBarProvider());
             if (this.inputHandler.getCliOptions().advanced.submissionCharsetOverride != null) {
